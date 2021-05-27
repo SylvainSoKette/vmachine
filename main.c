4b05c3f47a8a0098839aeb0fbf501c1d94c5b54f
Sylvain SoKette <sokette.dev@gmail.com>
2021-05-27 20:05:44 +0200

    move render stuff to the render.h header file

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 
 #include <SDL2/SDL.h>
 
-#define DEBUG       0
+#include "render.h"
+
+#define DEBUG       1
 #define DELAY       1
 #define MEMORY_SIZE 16
 
@@ -137,54 +139,7 @@ int get_bit_at(int number, int index)
 // DISPLAY STUFF //////////////////////////////////////////
 ///////////////////////////////////////////////////////////
 
-// Window Stuff
-const char* WINDOW_TITLE = "vmachine";
-const int WINDOW_WIDTH = 608;
-const int WINDOW_HEIGHT = 512;
-const Uint32 WINDOW_FLAGS = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL;
 
-typedef struct {
-    int r;
-    int g;
-    int b;
-    int a;
-} Color;
-
-Color COLOR_BLACK           = { .r = 0,   .g = 0,   .b = 0,   .a = 255, };
-Color COLOR_WHITE           = { .r = 230, .g = 230, .b = 230, .a = 255, };
-Color COLOR_WHITE_HIGHLIGHT = { .r = 230, .g = 230, .b = 255, .a = 255, };    
-Color COLOR_BLACK_HIGHLIGHT = { .r = 20,  .g = 20,  .b = 50,  .a = 255, };
-
-void init_sdl_window_renderer(SDL_Window **window, SDL_Renderer **renderer)
-{
-    if(SDL_Init(SDL_INIT_EVERYTHING))
-    {
-        printf("[ERROR] Could not initialize SDL.\n");
-    }
-
-    *window = SDL_CreateWindow(
-        WINDOW_TITLE,
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT,
-        WINDOW_FLAGS
-    );
-
-    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_SOFTWARE);
-}
-
-void draw_rect(SDL_Renderer* r, int x, int y, int w, int h, Color c)
-{
-    SDL_SetRenderDrawColor(r, c.r, c.g, c.b, c.a);
-    for (int dy = y; dy < y + h; dy++)
-    {
-        for (int dx = x; dx < x + w; dx++)
-        {
-            SDL_RenderDrawPoint(r, dx, dy);
-        }
-    }
-}
 
 void draw_program_counter(SDL_Renderer* r, int x, int y, int ppb)
 {
@@ -307,7 +262,6 @@ void wait_for_input_loop()
                 return;
             }
         }
-        SDL_Delay(10);
     }
 }
 
@@ -327,8 +281,6 @@ int main(int argc, char** argv)
 
     while (!EXIT)
     {
-        if (DELAY) SDL_Delay(333);
-
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
---
+++ render.h
@@ -0,0 +1,57 @@
+#ifndef __render_h
+#define __render_h
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+
+const char* WINDOW_TITLE = "vmachine";
+const int WINDOW_WIDTH = 608;
+const int WINDOW_HEIGHT = 512;
+const Uint32 WINDOW_FLAGS = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL;
+
+typedef struct {
+    int r;
+    int g;
+    int b;
+    int a;
+} Color;
+
+Color COLOR_BLACK           = { .r = 0,   .g = 0,   .b = 0,   .a = 255, };
+Color COLOR_WHITE           = { .r = 230, .g = 230, .b = 230, .a = 255, };
+Color COLOR_WHITE_HIGHLIGHT = { .r = 230, .g = 230, .b = 255, .a = 255, };    
+Color COLOR_BLACK_HIGHLIGHT = { .r = 20,  .g = 20,  .b = 50,  .a = 255, };
+
+void init_sdl_window_renderer(SDL_Window **window, SDL_Renderer **renderer)
+{
+    if(SDL_Init(SDL_INIT_EVERYTHING))
+    {
+        printf("[ERROR] Could not initialize SDL.\n");
+    }
+
+    *window = SDL_CreateWindow(
+        WINDOW_TITLE,
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WINDOW_FLAGS
+    );
+
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_SOFTWARE);
+}
+
+void draw_rect(SDL_Renderer* r, int x, int y, int w, int h, Color c)
+{
+    SDL_SetRenderDrawColor(r, c.r, c.g, c.b, c.a);
+    for (int dy = y; dy < y + h; dy++)
+    {
+        for (int dx = x; dx < x + w; dx++)
+        {
+            SDL_RenderDrawPoint(r, dx, dy);
+        }
+    }
+}
+
+#endif // __render_h
