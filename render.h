31ca4163ca0d8ba75d9be2cd8c3a928a374e58e9
Sylvain SoKette <sokette.dev@gmail.com>
2026-03-13 21:58:05 +0100

    fix typos and indent

--- render.h
+++ render.h
@@ -12,34 +12,33 @@ const int WINDOW_HEIGHT = 512;
 const Uint32 WINDOW_FLAGS = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL;
 
 typedef struct {
-    int r;
-    int g;
-    int b;
-    int a;
+	int r;
+	int g;
+	int b;
+	int a;
 } Color;
 
 Color COLOR_BLACK           = { .r = 0,   .g = 0,   .b = 0,   .a = 255, };
 Color COLOR_WHITE           = { .r = 230, .g = 230, .b = 230, .a = 255, };
-Color COLOR_WHITE_HIGHLIGHT = { .r = 230, .g = 230, .b = 255, .a = 255, };    
+Color COLOR_WHITE_HIGHLIGHT = { .r = 230, .g = 230, .b = 255, .a = 255, };
 Color COLOR_BLACK_HIGHLIGHT = { .r = 20,  .g = 20,  .b = 50,  .a = 255, };
 
 void init_sdl_window_renderer(SDL_Window **window, SDL_Renderer **renderer)
 {
-    if(SDL_Init(SDL_INIT_EVERYTHING))
-    {
-        printf("[ERROR] Could not initialize SDL.\n");
-    }
+	if (SDL_Init(SDL_INIT_EVERYTHING)) {
+		printf("[ERROR] Could not initialize SDL.\n");
+	}
 
-    *window = SDL_CreateWindow(
-        WINDOW_TITLE,
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT,
-        WINDOW_FLAGS
-    );
+	*window = SDL_CreateWindow(
+		WINDOW_TITLE,
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		WINDOW_FLAGS
+	);
 
-    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_SOFTWARE);
+	*renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_SOFTWARE);
 }
 
 inline void draw_rect(SDL_Renderer* r, int x, int y, int w, int h, Color c)
@@ -51,17 +50,8 @@ inline void draw_rect(SDL_Renderer* r, int x, int y, int w, int h, Color c)
 		.h = h
 	};
 
-    SDL_SetRenderDrawColor(r, c.r, c.g, c.b, c.a);	
+	SDL_SetRenderDrawColor(r, c.r, c.g, c.b, c.a);
 	SDL_RenderFillRect(r, &rect);
-	/*
-	for (int dy = y; dy < y + h; dy++)
-    {
-        for (int dx = x; dx < x + w; dx++)
-        {
-            SDL_RenderDrawPoint(r, dx, dy);
-        }
-    }
-	*/
 }
 
 #endif // __render_h
